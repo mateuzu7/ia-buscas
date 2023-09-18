b1b91a1a692f5ffdf86e0c33bd3aee6317be8d82
jstoquica <jstoquica@gmail.com>
2023-09-18 09:10:59 -0300

    salvar mundaças develop

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import espacoDeEstados.*;
 import estrategiasDeBusca.heuristica.*;
+import estrategiasDeBusca.cega.*;
 
 public class Main {
 
 	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) {
 		
-		//char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
-		//char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
-		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
-		//char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
+//		char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
+		char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
+//		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
+//		char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
+		
+		char[] cfgFim = {'1','2','3','4','5','6','7',' ','8'}; // OutOfMemory
+		
 
 		Puzzle8 puzzleInicial = new Puzzle8();
 		puzzleInicial.setEstado(cfgIni);
@@ -17,11 +21,18 @@ public class Main {
 		puzzleInicial.setAvaliacao( puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO) );
 			
 		Puzzle8 puzzleFinal = new Puzzle8();
-		puzzleFinal.setEstado( Puzzle8.TABULEIRO_ORGANIZADO );
+		puzzleFinal.setEstado( cfgFim );
 		puzzleFinal.setCusto(0);
 		puzzleFinal.setAvaliacao(0);
 						
+//		BuscaCega busca = new BuscaEmLargura();
+//		BuscaCega busca = new BuscaEmProfundidade();
+//		BuscaCega busca = new BuscaEmProfundidadeLimitada(null, null, 16);
+		
 		BuscaInformada busca = new AStar();
+//		BuscaInformada busca = new HillClimbing();
+//		BuscaInformada busca = new BestFirst();
+		
 		busca.setInicio(puzzleInicial);
 		busca.setObjetivo(puzzleFinal);
 		busca.buscar();
